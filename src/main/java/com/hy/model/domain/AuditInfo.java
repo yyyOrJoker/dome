131a9c3a2b8732fa04996bc163232cc055b8648b
yyy1867 <258123>
2017-01-05 15:25:29 +0800

    默认的

---
+++ src/main/java/com/hy/model/domain/AuditInfo.java
@@ -0,0 +1,62 @@
+package com.hy.model.domain;
+
+import javax.persistence.*;
+import java.util.Date;
+
+/**
+ * Created by yyy1867 on 2017/1/5.
+ */
+public class AuditInfo extends BaseDomain {
+
+    @ManyToOne(cascade = CascadeType.ALL)
+    @JoinColumn(name = "timesheet_id")
+    private CmTimesheet timesheet;
+
+    private int auditPresonId;
+
+    @Temporal(TemporalType.TIMESTAMP)
+    private Date date;
+
+    private String resultl;
+    private String advice;
+
+    public CmTimesheet getTimesheet() {
+        return timesheet;
+    }
+
+    public void setTimesheet(CmTimesheet timesheet) {
+        this.timesheet = timesheet;
+    }
+
+    public int getAuditPresonId() {
+        return auditPresonId;
+    }
+
+    public void setAuditPresonId(int auditPresonId) {
+        this.auditPresonId = auditPresonId;
+    }
+
+    public Date getDate() {
+        return date;
+    }
+
+    public void setDate(Date date) {
+        this.date = date;
+    }
+
+    public String getResultl() {
+        return resultl;
+    }
+
+    public void setResultl(String resultl) {
+        this.resultl = resultl;
+    }
+
+    public String getAdvice() {
+        return advice;
+    }
+
+    public void setAdvice(String advice) {
+        this.advice = advice;
+    }
+}
